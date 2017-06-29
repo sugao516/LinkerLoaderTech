749290cec2ea3a7b0e49309ec6705b3c6deb099c
sugao516 <sugao516.a@gmail.com>
2017-06-29 22:48:37 +0900

    リスト4.1

--- src/pointer/pointer.c
+++ src/pointer/pointer.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 char s1[] = "This is array.";
 char * s2 = "This is pointer.";
 
 int main()
 {
-  printf(" s1 = 0x%08x\n", (unsigned int) s1);
-  printf("&s1 = 0x%08x\n", (unsigned int)&s1);
-  printf(" s2 = 0x%08x\n", (unsigned int) s2);
-  printf("&s2 = 0x%08x\n", (unsigned int)&s2);
-  printf("sizeof(s1) = %d\n", sizeof(s1));
-  printf("sizeof(s2) = %d\n", sizeof(s2));
+  printf(" s1 = %p\n",  s1);
+  printf("&s1 = %p\n", &s1);
+  printf(" s2 = %p\n",  s2);
+  printf("&s2 = %p\n", &s2);
+  printf("sizeof(s1) = %ld\n", sizeof(s1));
+  printf("sizeof(s2) = %ld\n", sizeof(s2));
   exit (0);
 }
